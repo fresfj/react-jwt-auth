3d55cb7cf4febee3eea195fae13c1b2efe63c130
Francisco Freitas Jr <fresfj@gmail.com>
2019-07-17 15:36:54 -0300

    Alteração da Estutura do Projeto - components e util adicionados

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Estamos Trabalhando para o mundo!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
